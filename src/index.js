6a0aae42ef7e923b8f97f9571249ad6c48745c22
J-Skarbek <john@snarkmediaagency.com>
2022-11-15 12:01:09 -0600

    Setting up and configuring webpack.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import TestImg from './images/test-image.png';
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  const testImg = new Image();
+  testImg.src = TestImg;
+
+  element.appendChild(testImg);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
