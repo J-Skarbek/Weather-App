685d15c26311640131d5c7a2999ad0204c392f0f
J-Skarbek <john@snarkmediaagency.com>
2022-11-16 10:04:15 -0600

    Get the acutal weather and use Math.round() to remove the float.

--- src/getData.js
+++ src/getData.js
@@ -4,19 +4,19 @@ export function getGeocodingData() {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Nashville&appid=${dataKey}`,
     {mode: 'cors'})
     .then(function(response) {
-      console.log(response);
       return response.json();
     })
     .then(function(response) {
       console.log(`latitude = ${response[0].lat} and longitude is = ${response[0].lon}`)
       console.log(response);
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=${dataKey}`,
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=${dataKey}&units=imperial`,
       {mode: 'cors'})
       .then(function(response) {
         return response.json();
       })
       .then(function(response) {
         console.log(response)
+        console.log(`The temp is currently ${Math.round(response.main.temp)}.`)
       })
     })
 }
