ba8afb0170a8a7f662510fd24cd30242aae4f0a8
J-Skarbek <john@snarkmediaagency.com>
2022-12-02 13:22:47 -0600

    More styling and pulling in additional info from the api for the current weather display.

--- src/displayCurrentTemp.js
+++ src/displayCurrentTemp.js
@@ -5,6 +5,9 @@ export function displayCurrentTemp(response) {
   const tempDisplay = document.createElement('p');
   element.appendChild(tempDisplay);
   tempDisplay.innerText = `The temp is currently ${Math.round(response.main.temp)}.`
+  const feelsLikeDisplay = document.createElement('p');
+  feelsLikeDisplay.innerText = `Feels Like: ${Math.round(response.main.temp_max)}`
+  element.appendChild(feelsLikeDisplay)
   mainContent.appendChild(element);
   return element;  
 }
