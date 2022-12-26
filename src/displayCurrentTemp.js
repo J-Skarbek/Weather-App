b250a7445c5e8110a96dd56bc080732b8f55113e
J-Skarbek <john@snarkmediaagency.com>
2022-12-26 14:54:14 -0600

    Clean up code for resetting the current weather data on location lookup.

--- src/displayCurrentTemp.js
+++ src/displayCurrentTemp.js
@@ -1,8 +1,18 @@
 import { determineCurrentWeatherIcon } from './displayWeatherIcon.js';
 
+// const mainContent = document.querySelector('.main-content');
+// const currentWeatherDiv = document.createElement('div');
+// const currentLocation = document.createElement('h2');
+// const currentWeatherDescription = document.createElement('h3');
+// const currentDateTime = document.createElement('p');
+// const tempDisplay = document.createElement('p');
+// const feelsLikeDisplay = document.createElement('p');
+// const todaysHigh = document.createElement('p');
+// const todaysLow = document.createElement('p');
+
 export function displayCurrentTemp(response) {
   const mainContent = document.querySelector('.main-content');
-  const element = document.createElement('div');
+  const currentWeatherDiv = document.createElement('div');
   const currentLocation = document.createElement('h2');
   const currentWeatherDescription = document.createElement('h3');
   const currentDateTime = document.createElement('p');
@@ -10,9 +20,9 @@ export function displayCurrentTemp(response) {
   const feelsLikeDisplay = document.createElement('p');
   const todaysHigh = document.createElement('p');
   const todaysLow = document.createElement('p');
-  element.classList.add('current-temp');
-  element.append(currentLocation, currentWeatherDescription, currentDateTime, tempDisplay, feelsLikeDisplay, todaysHigh, todaysLow, determineCurrentWeatherIcon(response))
-  mainContent.appendChild(element);
+  currentWeatherDiv.classList.add('current-temp');
+  currentWeatherDiv.append(currentLocation, currentWeatherDescription, currentDateTime, tempDisplay, feelsLikeDisplay, todaysHigh, todaysLow, determineCurrentWeatherIcon(response))
+  mainContent.appendChild(currentWeatherDiv);
   currentLocation.innerText = `${response.name}`;
   currentWeatherDescription.innerText = `${response.weather[0].description}`;
   // currentDateTime.innerText = `dummy text`;
@@ -20,19 +30,10 @@ export function displayCurrentTemp(response) {
   feelsLikeDisplay.innerText = `Feels Like: ${Math.round(response.main.feels_like)}.`;
   todaysHigh.innerText = `Today's High: ${Math.round(response.main.temp_max)}.`;
   todaysLow.innerText = `Today's Low: ${Math.round(response.main.temp_min)}.`;
-  return element;  
+  return currentWeatherDiv;  
 }
 
 export function resetCurrentTempDisplay() {
-  const getCurrentWeatherHeaders = document.querySelectorAll('.current-temp h2, .current-temp h3');
-  getCurrentWeatherHeaders.forEach(header => {
-    header.innerText = '';
-  });
-
-  const getCurrentTempValues = document.querySelectorAll('.current-temp p');
-  getCurrentTempValues.forEach(currentTempValue => {
-    currentTempValue.innerText = '';
-  });
-  const resetCurrentWeatherIcon = document.querySelector('.current-temp img');
-  resetCurrentWeatherIcon.remove();
+  const getCurrentWeatherDiv = document.querySelector('.current-temp');
+  getCurrentWeatherDiv.remove();
 }
